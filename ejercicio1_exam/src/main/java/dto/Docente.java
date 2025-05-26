877a850f5c1bfe3fb501441b021858dd14071382
rosar <rosar@Rosario>
2025-05-26 15:05:38 -0500

    dto

---
+++ ejercicio1_exam/src/main/java/dto/Docente.java
@@ -0,0 +1,80 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dto;
+
+/**
+ *
+ * @author rosar
+ */
+public class Docente {
+    private String codigo;
+    private String nombre;
+    private String direccion;
+    private String fecha;
+    private double talla;
+    private String correo;
+
+    public Docente() {
+    }
+
+    public Docente(String codigo, String nombre, String direccion, String fecha, double talla, String correo) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.fecha = fecha;
+        this.talla = talla;
+        this.correo = correo;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public double getTalla() {
+        return talla;
+    }
+
+    public void setTalla(double talla) {
+        this.talla = talla;
+    }
+    
+    
+}
