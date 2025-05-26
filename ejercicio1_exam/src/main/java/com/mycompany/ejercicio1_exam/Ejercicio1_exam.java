a9ca225d60a40c79a595dac034e54ba9d06fc0f9
tata <0333232012@unjfsc.edu.pe>
2025-05-26 14:40:10 -0500

    Add files via upload

---
+++ ejercicio1_exam/src/main/java/com/mycompany/ejercicio1_exam/Ejercicio1_exam.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio1_exam;
+
+/**
+ *
+ * @author rosar
+ */
+public class Ejercicio1_exam {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
