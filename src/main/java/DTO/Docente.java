ce653aff48d363aaae6cc7f28d8b237535191145
rosar <rosar@Rosario>
2025-05-26 14:47:22 -0500

    ob

---
+++ src/main/java/DTO/Docente.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DTO;
+
+/**
+ *
+ * @author rosar
+ */
+public class Docente {
+    private String codigo;
+    private String nombre;
+    private String direccion;
+    
+    
+}
